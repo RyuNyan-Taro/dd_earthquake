5ce91c73b5c2d9453a79755305e60dde413c56e2
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-02-22 08:20:22 +0900

    add docstring and modified Error message

--- src/common/file/_read.py
+++ src/common/file/_read.py
@@ -3,8 +3,19 @@ from pathlib import Path
 
 
 def read_data(data_suffix: str) -> pd.DataFrame or ValueError:
-    """Return data which selected data_suffix."""
+    """
+    Return data which selected data_suffix.
 
+    Parameters
+    ----------
+    data_suffix : {''train', 'test', 'submission'}, optional
+        What data is returned
+
+    Returns
+    -------
+    DataFrame of selected as data_suffix.
+
+    """
     DATA_DIR: Path = Path('..', '..', '..', 'data', 'final', 'public')
     if data_suffix == 'train':
         return pd.read_csv(DATA_DIR / 'train_values.csv', index_col='building_id'), \
@@ -15,4 +26,4 @@ def read_data(data_suffix: str) -> pd.DataFrame or ValueError:
         return pd.read_csv(DATA_DIR / 'submission_format.csv', index_col='building_id')
 
     # Return error if other data_suffix is selected
-    raise ValueError('You must select {train, test} as train_test.')
+    raise ValueError('You must select {train, test, submission} as train_test.')
