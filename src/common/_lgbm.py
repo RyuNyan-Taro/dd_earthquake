19a8ea6c737ed7937eb42c4376cf5b3801bf8ee3
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-02-22 21:32:30 +0900

    deleted unused module

--- src/common/_lgbm.py
+++ src/common/_lgbm.py
@@ -4,10 +4,8 @@ import pandas as pd
 import numpy as np
 
 import lightgbm as lgb
-from pandas._typing import NDFrameT
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 
 from . import file
